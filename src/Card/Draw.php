40a63a3b67d40077400e660f234ab4607580a91e
Tomoko Ishii <tomoko.s.ishii@gmail.com>
2023-04-14 20:09:24 +0200

    update

--- src/Card/Draw.php
+++ src/Card/Draw.php
@@ -4,21 +4,21 @@ namespace App\Card;
 
 class Draw extends Card
 {
-    public array $draw = [];
-    public $counter = 52;
+    // public array $draw = [];
+    // public $counter = 52;
 
-    public function getDraw(int $num=1)
-    {
-        $deck = new \App\Card\Deck();
-        $deckArr = $deck->getDeck();
-        $i = random_int(0, 51);
-        $this->draw = $deckArr[$i];
-        return $this->draw;
-    }
+    // public function getDraw(int $num=1)
+    // {
+    //     $deck = new \App\Card\Deck();
+    //     $deckArr = $deck->getDeck();
+    //     $i = random_int(0, 51);
+    //     $this->draw = $deckArr[$i];
+    //     return $this->draw;
+    // }
 
-    public function counter($count)
-    {
-        $cardsLeft = $count - 1;
-        return $cardsLeft;
-    }
+    // public function counter($count)
+    // {
+    //     $cardsLeft = $count - 1;
+    //     return $cardsLeft;
+    // }
 }
