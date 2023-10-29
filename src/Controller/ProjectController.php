1248e8974a37b59b9014c3e721fae58787b11892
Tomoko Ishii <tomoko.s.ishii@gmail.com>
2023-10-29 10:15:31 +0100

    project api updated

--- src/Controller/ProjectController.php
+++ src/Controller/ProjectController.php
@@ -4,8 +4,10 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
+
 use App\Entity\Share;
 use App\Entity\Cloud;
 use Doctrine\Persistence\ManagerRegistry;
@@ -62,12 +64,7 @@ class ProjectController extends AbstractController
             'gcp' => $gcp,
         ]);
     }
-    // #[Route("/proj", name: "project")]
-    // public function showShare(
-    // ): Response {
-        
-    //     return $this->render('project.html.twig');
-    // }
+
     #[Route("/proj/about", name: "project-about")]
     public function showAbout(): Response {
 
@@ -83,8 +80,7 @@ class ProjectController extends AbstractController
 
     #[Route("/proj/api", name: "project-api")]
     public function showAPI(
-    ): Response {
-        
+    ): Response {  
         return $this->render('project/api.html.twig');
     }
 
@@ -94,5 +90,134 @@ class ProjectController extends AbstractController
     ): Response {
         $data = $ShareRepository->findAll();
         return new JsonResponse($data);
+    }
+
+    #[Route("/proj/api/aws", name: "project-api-aws", methods: ['GET'])]
+    public function getAPIAws(
+        CloudRepository $cloudRepository
+    ): JsonResponse {
+        $awsData = $cloudRepository->findOneBy(['owner' => 'Amazon']); // Assuming 'owner' is the field name in your Cloud entity
+        if (!$awsData) {
+            return new JsonResponse(['error' => 'Data not found'], Response::HTTP_NOT_FOUND);
+        }
+
+        $data = [
+            'share' => $awsData->getShare(),
+            'launch' => $awsData->getLaunch(),
+            'owner' => $awsData->getOwner(),
+            'revenue' => $awsData->getRevenue(),
+            'pricing' => $awsData->getPricing(),
+            'storage' => $awsData->getStorage(),
+            'vn' => $awsData->getVn(),
+            'api' => $awsData->getApi(),
+        ];
+
+        return new JsonResponse($data);
+    }
+
+    #[Route("/proj/api/azure", name: "project-api-azure", methods: ['GET'])]
+    public function getAPIAzure(
+        CloudRepository $cloudRepository
+    ): JsonResponse {
+        $azureData = $cloudRepository->findOneBy(['owner' => 'Microsoft']); // Assuming 'owner' is the field name in your Cloud entity for Azure
+        if (!$azureData) {
+            return new JsonResponse(['error' => 'Data not found'], Response::HTTP_NOT_FOUND);
+        }
+
+        $data = [
+            'share' => $azureData->getShare(),
+            'launch' => $azureData->getLaunch(),
+            'owner' => $azureData->getOwner(),
+            'revenue' => $azureData->getRevenue(),
+            'pricing' => $azureData->getPricing(),
+            'storage' => $azureData->getStorage(),
+            'vn' => $azureData->getVn(),
+            'api' => $azureData->getApi(),
+        ];
+
+        return new JsonResponse($data);
+    }
+    #[Route("/proj/api/gcp", name: "project-api-gcp", methods: ['GET'])]
+    public function getAPIGCP(
+        CloudRepository $cloudRepository
+    ): JsonResponse {
+        $gcpData = $cloudRepository->findOneBy(['owner' => 'Google']); // Assuming 'owner' is the field name in your Cloud entity for GCP
+        if (!$gcpData) {
+            return new JsonResponse(['error' => 'Data not found'], Response::HTTP_NOT_FOUND);
+        }
+
+        $data = [
+            'share' => $gcpData->getShare(),
+            'launch' => $gcpData->getLaunch(),
+            'owner' => $gcpData->getOwner(),
+            'revenue' => $gcpData->getRevenue(),
+            'pricing' => $gcpData->getPricing(),
+            'storage' => $gcpData->getStorage(),
+            'vn' => $gcpData->getVn(),
+            'api' => $gcpData->getApi(),
+        ];
+
+        return new JsonResponse($data);
+    }
+
+    #[Route("/proj/api/all", name: "project-api-all", methods: ['GET'])]
+    public function getAPICloud(
+        CloudRepository $cloudRepository
+    ): JsonResponse {
+        $providers = ['Amazon', 'Microsoft', 'Google'];
+        $data = [];
+
+        foreach ($providers as $provider) {
+            $providerData = $cloudRepository->findOneBy(['owner' => $provider]);
+
+            if (!$providerData) {
+                return new JsonResponse(['error' => "$provider data not found"], Response::HTTP_NOT_FOUND);
+            }
+
+            $data[$provider] = [
+                'share' => $providerData->getShare(),
+                'launch' => $providerData->getLaunch(),
+                'owner' => $providerData->getOwner(),
+                'revenue' => $providerData->getRevenue(),
+                'pricing' => $providerData->getPricing(),
+                'storage' => $providerData->getStorage(),
+                'vn' => $providerData->getVn(),
+                'api' => $providerData->getApi(),
+            ];
+        }
+
+        return new JsonResponse($data);
+    }
+
+    #[Route("/proj/api/aws/update", name: "project-api-aws-update", methods: ["POST", "GET"])]
+    public function updateAPIAWS(
+        Request $request,
+        CloudRepository $cloudRepository,
+        ManagerRegistry $doctrine
+    ): JsonResponse {
+        $newShareValue = $request->request->get('new');
+
+        if ($newShareValue !== null) {
+            $entityManager = $doctrine->getManager();
+            $aws = $cloudRepository->findOneBy(['owner' => 'Amazon']);
+    
+            $aws->setShare($newShareValue);
+            $entityManager->flush();
+
+            $awsData = $cloudRepository->findOneBy(['owner' => 'Amazon']);
+
+            $data = [
+                'share' => $awsData->getShare(),
+                'launch' => $awsData->getLaunch(),
+                'owner' => $awsData->getOwner(),
+                'revenue' => $awsData->getRevenue(),
+                'pricing' => $awsData->getPricing(),
+                'storage' => $awsData->getStorage(),
+                'vn' => $awsData->getVn(),
+                'api' => $awsData->getApi(),
+            ];
+    
+            return new JsonResponse($data);
+        }
     }
 }
